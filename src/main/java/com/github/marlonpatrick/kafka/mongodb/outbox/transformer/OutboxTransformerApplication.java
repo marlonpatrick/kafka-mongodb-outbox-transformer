125272f33c49698dbecb11aa9e45980a185b7c9b
Marlon Patrick <marlonpatrick10@gmail.com>
2020-11-28 13:54:23 -0300

    feat: kafka streams settings fine tune

--- src/main/java/com/github/marlonpatrick/kafka/mongodb/outbox/transformer/OutboxTransformerApplication.java
+++ src/main/java/com/github/marlonpatrick/kafka/mongodb/outbox/transformer/OutboxTransformerApplication.java
@@ -1,23 +1,17 @@
 package com.github.marlonpatrick.kafka.mongodb.outbox.transformer;
 
-import java.util.HashMap;
-import java.util.Map;
 import java.util.regex.Pattern;
 
-import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.streams.StreamsBuilder;
-import org.apache.kafka.streams.StreamsConfig;
 import org.apache.kafka.streams.kstream.Consumed;
 import org.apache.kafka.streams.kstream.KStream;
+import org.apache.kafka.streams.kstream.Printed;
 import org.apache.kafka.streams.kstream.Produced;
-import org.apache.kafka.streams.processor.WallclockTimestampExtractor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.env.Environment;
 import org.springframework.kafka.annotation.EnableKafkaStreams;
-import org.springframework.kafka.annotation.KafkaStreamsDefaultConfiguration;
-import org.springframework.kafka.config.KafkaStreamsConfiguration;
 import org.springframework.kafka.support.serializer.JsonSerde;
 
 @SpringBootApplication
@@ -28,20 +22,17 @@ public class OutboxTransformerApplication {
 		SpringApplication.run(OutboxTransformerApplication.class, args);
 	}
 
-	@Bean(name = KafkaStreamsDefaultConfiguration.DEFAULT_STREAMS_CONFIG_BEAN_NAME)
-	public KafkaStreamsConfiguration kStreamsConfigs() {
-		Map<String, Object> props = new HashMap<>();
-		props.put(StreamsConfig.APPLICATION_ID_CONFIG, "kafka-mongodb-outbox-transformer-x");
-		props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
-		props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
-		props.put(StreamsConfig.DEFAULT_TIMESTAMP_EXTRACTOR_CLASS_CONFIG, WallclockTimestampExtractor.class.getName());
-		return new KafkaStreamsConfiguration(props);
-	}
-
 	@Bean
 	public KStream<String, RawOutboxDocument> kStream(StreamsBuilder kStreamBuilder, Environment env) {
-		KStream<String, RawOutboxDocument> stream = kStreamBuilder.stream(Pattern.compile(env.getProperty("outbox.transformer.kafka.consumer.topic.pattern")),
+
+		String inputTopicPattern = env.getProperty("outbox.transformer.kafka.consumer.topic.pattern");
+
+		KStream<String, RawOutboxDocument> stream = kStreamBuilder.stream(Pattern.compile(inputTopicPattern),
 				Consumed.with(null, new JsonSerde<>(RawOutboxDocument.class)));
+
+		if (env.getProperty("outbox.transformer.kafka.streams.log.sysout", Boolean.class)) {
+			stream.print(Printed.toSysOut());
+		}
 
 		stream.flatMapValues(completeDocument -> {
 
@@ -51,10 +42,8 @@ public class OutboxTransformerApplication {
 			}
 
 			return completeDocument.fullDocument.outbox;
-		})
-		.to((k, v, recordContext) -> v.targetTopic, Produced.valueSerde(new JsonSerde<>(OutboxMessage.class)));
+		}).to((k, v, recordContext) -> v.targetTopic, Produced.valueSerde(new JsonSerde<>(OutboxMessage.class)));
 
 		return stream;
 	}
 }
-
